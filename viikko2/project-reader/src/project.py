084a753c9877f37f9cc0027d6d0b96ecb63252f9
tuome <tonni@tuta.io>
2023-11-10 15:23:46 +0200

    Add week 2 exercise 2

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,21 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, license, authors, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.license = license
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
     def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+        return "\n-".join(dependencies) if len(dependencies) > 0 else "-"
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
+            f"\nLicense: {self.license}\n"
+            f"\nAuthors: {self._stringify_dependencies(self.authors)}\n"
             f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
             f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
         )
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import toml
 
 
 class ProjectReader:
@@ -9,7 +10,18 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        #print(content)
+
+        deserialized = toml.loads(content)
+        
+        name = deserialized['tool']['poetry']['name']
+        description = deserialized['tool']['poetry']['description']
+        license = deserialized['tool']['poetry']['license']
+
+        authors = deserialized['tool']['poetry']['authors']
+
+        dependencies = deserialized['tool']['poetry']['dependencies']
+        dev_dependencies = deserialized['tool']['poetry']['group']['dev']['dependencies']
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(name, description, license, authors, dependencies, dev_dependencies)
