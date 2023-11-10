6cc49e291b23e71ec9884c0696a8eb9e898c5f40
tuome <tonni@tuta.io>
2023-11-10 21:23:13 +0200

    Add week 2 exercise 10

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,26 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    print(response)
+
+
+    players = []
+
+    for player_dict in response:
+        if(player_dict['nationality'] == 'FIN'):
+            player = Player(player_dict)
+            players.append(player)
+
+    print("Oliot:")
+
+    for player in players:
+        print(player)
+
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,15 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.nationality = dict['nationality']
+        self.assists = dict['assists']
+        self.goals = dict['goals']
+        self.penalties = dict['penalties']
+        self.team = dict['team']
+        self.games = dict['games']
+
+    def get_nationality(self):
+        return self.nationality
     
     def __str__(self):
-        return self.name
+        return f'{self.name} team {self.team}  goals {self.goals} assists {self.assists}'
